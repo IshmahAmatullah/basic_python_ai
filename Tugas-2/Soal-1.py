af6871a94621515b0eecbdf5568a80df0da9d7ef
unknown <ishmah.sherlock01@gmail.com>
2021-03-24 20:33:38 +0700

    Pertemuan Keempat

--- Tugas-2/Soal-1.py
+++ Tugas-2/Soal-1.py
@@ -1,35 +1,8 @@
-# nama = []
-# nomor = []
-# z = 0
+import sys
 
-# n = int(input("Jumlah Kontak :"))
-# for x in range (n):
-
-# def show_menu():
-#     print(" Menu ")
-#     print("1. Daftar Kontak")
-#     print("2. Tambah Kontak")
-#     print("3. Keluar")
-    
-#     menu = input("Pilih menu :")
-#     print("\n")
-    
-#     if menu == 1:
-#         show_data()
-#     elif menu == 2:
-#         insert_data()
-#     elif menu == 3:
-#         print("Program selesai, sampai jumpa!")
-#     else:
-#         print("Menu tidak tersedia")
-#     return show_menu()
-
-#     def show_data():
-
-import csv
-import os
-
-csv_filename = 'contacts.csv'
+contacts_name = []
+contacts_phone = []
+y = 0
 
 def show_menu():
     print("------Menu------")
@@ -38,6 +11,7 @@ def show_menu():
     print("3. Keluar")
     print("----------------")
     selected_menu = input("Pilih menu: ")
+    print("\n")
 
     if(selected_menu == "1"):
         show_contact()
@@ -48,43 +22,24 @@ def show_menu():
         exit()
     else:
         print("Menu tidak tersedia")
-        back_to_menu()
-
-def back_to_menu():
-    print("\n")
-    input("Tekan Enter untuk kembali...")
-    show_menu()
 
 def show_contact():
-    contacts = []
-    with open(csv_filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        for row in csv_reader:
-            contacts.append(row)
-        
-    if(len(contacts) > 0):
-        labels = contacts.pop(0)
-        print(f"{labels[0]} \t {labels[1]} \t\t {labels[2]}")
-        print("-"*34)
-        for data in contacts:
-            print(f'{data[0]} \t {data[1]} \t {data[2]}')
-    else:
+    if(len(contacts_name) <= 0):
         print("Tidak ada data!")
-    back_to_menu()
+    else:
+        for indeks in range(len(contacts_name)):
+            print("[%d] %s" % (indeks+1, contacts_name[indeks]))
+            for y in range(len(contacts_phone)):
+                print("--->", contacts_phone[y])
+                y += 1
 
 def create_contact():
-    with open(csv_filename, mode='a') as csv_file:
-        fieldnames = ['Nama', 'Telepon']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        nama = input("Nama : ")
-        telepon = input("No. Telepon : ")
-
-        writer.writerow({'Nama' : nama, 'Telepon' : telepon})
-        print("Kontak berhasil ditambahkan")
-        
-    back_to_menu()
+    nama = input("Nama : ")
+    contacts_name.append(nama)
+    telpon = input("No Telepon : ")
+    contacts_phone.append(telpon)
+    print("Kontak berhasil ditambahkan")
 
 if __name__ == "__main__":
-    while True:
+    while(True):
         show_menu()
